d4dbaaf45342f23c6fc42755513691d497c0c5c8
monis07 <monisazeem@gmail.com>
2024-10-01 23:35:35 +0530

    minor changes

--- admin.js
+++ admin.js
@@ -1,14 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express';
 const router = express.Router();
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const genAI = new GoogleGenerativeAI('process.env.GOOGLE_API_KEY');
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 
 router.post('/execute',async (req,res)=>{
     const img = req.body.img;
     const prompt = req.body.prompt;
-    console.log(req.body.prompt)
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+    console.log("prompt",req.body.prompt)
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
     const result = await model.generateContent([
 prompt,
 {inlineData: {data: img,
